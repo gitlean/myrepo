19397fe5b569b502581d06b2ce4eaf4f3220259c
tom <tom@example.com>
2017-10-25 22:10:46 +0800

    commit

--- SS2H-SSecurity/src/main/java/com/xxx/security/LoginSuccessHandler.java
+++ SS2H-SSecurity/src/main/java/com/xxx/security/LoginSuccessHandler.java
@@ -6,6 +6,9 @@ import java.util.Iterator;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,13 +20,16 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
  */
 public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+	private static final Logger logger=LoggerFactory.getLogger(LoginSuccessHandler.class);
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 
 		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-		
+		logger.debug("debug sth....");
+		logger.info("LoginSuccessHandler..........");
+		logger.error("error sth ...");
 		// 输出登录提示信息
 		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>");
 		System.out.println("管理员 " + userDetails.getUsername() + " 登录");
