c224f0546283cbad5a578843af6506630aa63b33
jerry <jerry@example.com>
2017-12-04 18:14:30 +0800

    commit

--- spring-cxf/src/main/java/com/isoft/client/ws/HelloServiceClient.java
+++ spring-cxf/src/main/java/com/isoft/client/ws/HelloServiceClient.java
@@ -20,7 +20,8 @@ public class HelloServiceClient {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext-client.xml");
 		
-		//获取WS的本地代理
+		//获取WS的本地代理  
+		//IHelloService 应该是 通过wsdl2java生成的本地代码,这里由于和service端放在一块，直接用
 		IHelloService proxy = (IHelloService) context.getBean("client");
 		String response = proxy.sayHello("Peter");
 		System.out.println(response);
