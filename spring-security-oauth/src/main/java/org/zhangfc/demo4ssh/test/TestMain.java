08e1ba0d62aa37b016ccd28cc532edb03d63d3a7
jerry <jerry@example.com>
2018-01-07 14:54:24 +0800

    spring security oauth

---
+++ spring-security-oauth/src/main/java/org/zhangfc/demo4ssh/test/TestMain.java
@@ -0,0 +1,13 @@
+package org.zhangfc.demo4ssh.test;
+
+import java.util.UUID;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(UUID.randomUUID().toString());
+
+	}
+
+}
