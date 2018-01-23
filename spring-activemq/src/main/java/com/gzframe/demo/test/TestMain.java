33322fb21d8ae659fd95692e0a831e1380cb8915
jerry <jerry@example.com>
2018-01-23 09:10:53 +0800

    ActiveMQ

---
+++ spring-activemq/src/main/java/com/gzframe/demo/test/TestMain.java
@@ -0,0 +1,11 @@
+package com.gzframe.demo.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 javax.jms.JMSContext jms=null;
+
+	}
+
+}
