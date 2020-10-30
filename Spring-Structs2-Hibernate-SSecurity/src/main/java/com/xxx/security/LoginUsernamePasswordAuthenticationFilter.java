1e10cdc60dfdc9f9b60ec90eed51054a8c7405b9
david <david@example.com>
2020-10-30 21:20:58 +0800

    delete

--- Spring-Structs2-Hibernate-SSecurity/src/main/java/com/xxx/security/LoginUsernamePasswordAuthenticationFilter.java
+++
@@ -1,7 +0,0 @@
-package com.xxx.security;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-public class LoginUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-}
